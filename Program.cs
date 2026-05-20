665a44f4aff1987834f42b1f42a63f51a70221ee
Ulises <ufrixione@uamv.edu.ni>
2026-05-20 11:38:07 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿/*Leer 25 edades y realizar lo siguiente: 1. ingresar edades 2. mostratr edades
+3. Mostratr mayores de edad , 4. Mostrar menores de edad*/
+//1. Ingresar edades
+
+//2.Mostrar edades
+
+//3. Mayores de edad  
+
+//4. Menores de edad
