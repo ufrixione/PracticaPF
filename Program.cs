874647b75e069b6f43f831171d5120ff8d6bf1eb
gabrielito <gomcarrillo@uamv.edu.ni>
2026-05-20 12:29:48 -0600

    feat: ingresar edades

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
-﻿/*Leer 25 edades y realizar lo siguiente: 1. ingresar edades 2. mostratr edades
-3. Mostratr mayores de edad , 4. Mostrar menores de edad*/
+﻿/*Leer 25 edades y realizar lo siguiente: 1. ingresar edades 2. mostrar edades
+3. Mostrar mayores de edad , 4. Mostrar menores de edad*/
 //1. Ingresar edades
 
 //2.Mostrar edades
@@ -7,3 +7,20 @@
 //3. Mayores de edad  
 
 //4. Menores de edad
+
+int[] edades = new int[25];
+for (int i = 0; i < 25; i++)
+{
+try 
+{
+Console.Write("Ingrese la edad " + (i + 1) + ": ");
+string entrada = Console.ReadLine();
+edades[i] = int.Parse(entrada);
+}
+catch (Exception)
+{
+Console.WriteLine("¡Error! Debes ingresar un número válido.");
+i--;
+}
+}
+Console.WriteLine("Numeros guardados correctamente.");
