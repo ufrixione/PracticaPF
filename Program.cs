c7918b0370b270864bcd810d7ec21c4ee627f904
MoisesMoraga <momoraga@uamv.edu.ni>
2026-05-20 11:29:16 -0700

    feat: Agregar mostrar edades

--- Program.cs
+++ Program.cs
@@ -5,7 +5,10 @@ int [] edades = new int[25];
 //1. Ingresar edades
 
 //2.Mostrar edades
-
+for (int i = 0; i < edades.Length; i++)
+{
+    Console.WriteLine($"Edad {i + 1}: {edades[i]}");
+}
 //3. Mayores de edad  
 
 //4. Menores de edad
